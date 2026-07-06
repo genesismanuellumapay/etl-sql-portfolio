f812a350b2af3b1c5d89f2d348dd0a33803d3b3f
genesismanuellumapay <genesismanuellumapay.gmail.com>
2026-07-06 18:58:00 +0800

    added sql scripts in creating the tables to be used for Vendor Quarantine project

---
+++ 03_IICS_Projects/05_Vendor_Data_Quarantine_Pipeline_(Data Quality)/data/vendor_quarantine_table_scripts.sql
@@ -0,0 +1,31 @@
+--Lookup Reference table
+CREATE TABLE ref_category (
+    category_code VARCHAR(10) PRIMARY KEY,
+    category_name VARCHAR(50) NOT NULL
+);
+
+INSERT INTO ref_category (category_code, category_name) VALUES
+('ELEC', 'Electronics'),
+('FURN', 'Furniture'),
+('OFFC', 'Office Supplies');
+
+--Target table
+CREATE TABLE dim_product (
+    product_sk SERIAL PRIMARY KEY,
+    vendor_sku VARCHAR(50) NOT NULL,
+    product_name VARCHAR(100),
+    category_code VARCHAR(10),
+    unit_price DECIMAL(10,2),
+    stock_qty INT,
+    load_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+--Quarantine table
+CREATE TABLE err_vendor_products (
+    error_id SERIAL PRIMARY KEY,
+    vendor_sku VARCHAR(50),
+    raw_record_data TEXT,
+    error_reason VARCHAR(255),
+    error_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
